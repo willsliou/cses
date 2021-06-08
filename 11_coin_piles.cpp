1c37b537f1ab012ed11834488b46a6e89fb40556
Wills Liou <willsliou@protonmail.com>
2021-06-07 22:31:41 -0700

    coin_piles

---
+++ 11_coin_piles.cpp
@@ -0,0 +1,54 @@
+/*
+
+*/
+
+/*
+https://cses.fi/problemset/task/1754
+You have two coin piles containing a and b coins. On each move, you can either remove one coin from the left pile and two coins from the right pile, or two coins from the left pile and one coin from the right pile.
+
+Your task is to efficiently find out if you can empty both the piles.
+
+Input
+
+The first input line has an integer t: the number of tests.
+
+After this, there are t lines, each of which has two integers a and b: the numbers of coins in the piles.
+
+Output
+
+For each test, print "YES" if you can empty the piles and "NO" otherwise.
+
+Constraints
+1≤t≤105
+0≤a,b≤109
+Example
+
+Input:
+3
+2 1
+2 2
+3 3
+
+Output:
+YES
+NO
+YES
+*/
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+#define ll long long
+
+int main() {
+
+
+    ll n, ans = 0;
+    cin >> n;
+
+
+} // main
